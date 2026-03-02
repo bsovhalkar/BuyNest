2433df474819b17e52f4c2bebfd24fd8c87156e5
B S Ovhalkar <bhavaneshsovhalkar@gmail.com>
2026-03-02 22:20:11 +0530

    Initial commit

---
+++ src/main/java/com/ecommerse/BuyNest/BuyNestApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerse.BuyNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerse/BuyNest/BuyNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerse.BuyNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
